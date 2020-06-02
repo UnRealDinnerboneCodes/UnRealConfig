96ebc5671764393d746ca4f3ea6765049b32c0f8
UnReal Dinnerbone <unrealdinnerbone@gmail.com>
2020-06-02 12:05:51 -0500

    Changes to ConfigManger

--- src/main/java/com/unrealdinnerbone/config/ConfigManager.java
+++ src/main/java/com/unrealdinnerbone/config/ConfigManager.java
@@ -2,9 +2,13 @@ package com.unrealdinnerbone.config;
 
 import com.unrealdinnerbone.config.api.IConfig;
 import com.unrealdinnerbone.config.api.provider.IProvider;
+import com.unrealdinnerbone.config.impl.provider.EnvProvider;
+import com.unrealdinnerbone.config.impl.provider.FileProvider;
 import com.unrealdinnerbone.config.lib.ConfigUtils;
+import sun.security.krb5.Config;
 
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +16,6 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ConfigManager {
-
 
     private final List<IProvider> providers;
     private boolean doSave;
@@ -22,12 +25,13 @@ public class ConfigManager {
         this.doSave = true;
     }
 
-    public void addProvider(IProvider provider) {
+    public ConfigManager addProvider(IProvider provider) {
         this.providers.add(provider);
+        return this;
     }
 
 
-    public void saveConfig(IConfig iConfig) {
+    public <T extends IConfig> T saveConfig(T iConfig) {
         if(doSave) {
             Arrays.stream(iConfig.getClass().getDeclaredFields()).forEach(field -> {
                 Object value = ConfigUtils.getValueOfField(field, iConfig);
@@ -36,21 +40,37 @@ public class ConfigManager {
                         forEach(provider -> provider.save( iConfig.getFolderName(), iConfig.getName(), field.getName(),  value));
             });
         }
+        return iConfig;
     }
 
-    public void loadConfig(IConfig iConfig) {
+
+    public <T extends IConfig> IConfig[] loadConfigs(T... iConfigs) {
+        Arrays.stream(iConfigs).forEach(this::loadConfig);
+        return iConfigs;
+    }
+
+    public <T extends IConfig> IConfig loadConfig(T iConfig) {
         Arrays.stream(iConfig.getClass().getDeclaredFields()).forEach(field ->
                  findValueFor().stream()
                          .map(provider -> provider.get(iConfig.getFolderName(), iConfig.getName(), field.getName()))
                 .filter(Objects::nonNull)
                 .findFirst().ifPresent(o -> ConfigUtils.setValueIfField(field, iConfig, o)));
+        return iConfig;
     }
 
     private ArrayList<IProvider> findValueFor() {
         return providers.stream().filter(IProvider::canRead).collect(Collectors.toCollection(ArrayList::new));
     }
 
-    public void doSaving(boolean doSave) {
+    public ConfigManager doSaving(boolean doSave) {
         this.doSave = doSave;
+        return this;
+    }
+
+    public static ConfigManager createSimplePropertyConfigManger() {
+        return new ConfigManager().doSaving(false).addProvider(FileProvider.FILE_PROVIDER).addProvider(EnvProvider.ENV_PROVIDER);
+    }
+    public static ConfigManager createSimpleEnvPropertyConfigManger() {
+        return new ConfigManager().doSaving(false).addProvider(EnvProvider.ENV_PROVIDER);
     }
 }
