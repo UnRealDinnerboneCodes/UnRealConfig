930525b1633d109ef0157c62458d74a92a0f2721
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2021-12-15 11:53:45 -0600

    added is

--- src/main/java/com/unrealdinnerbone/config/api/Namespace.java
+++ src/main/java/com/unrealdinnerbone/config/api/Namespace.java
@@ -9,4 +9,8 @@ public record Namespace(String key, String value) {
     public static Namespace of(String key, String value) {
         return new Namespace(key, value);
     }
+
+    public boolean is(String key, String value) {
+        return toString().toLowerCase().equals(key.toLowerCase() + "." + value.toLowerCase());
+    }
 }
