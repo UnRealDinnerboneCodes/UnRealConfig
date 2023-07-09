8cc258153ba8e808b6c85137c7259fee64ab8027
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2023-07-09 17:50:26 -0500

    Fix Test

--- src/core/test/com/unrealdinnerbone/test/DefaultProviderTest.java
+++ src/core/test/com/unrealdinnerbone/test/DefaultProviderTest.java
@@ -13,12 +13,12 @@ public class DefaultProviderTest {
         ConfigManager configManager = new ConfigManager(new DefaultConfigProvider());
         TestConfig test = configManager.loadConfig("test", TestConfig::new);
         try {
-            Assertions.assertEquals("Hello", test.stringConfig.getExceptionally());
-            Assertions.assertEquals(true, test.booleanConfig.getExceptionally());
-            Assertions.assertEquals(1.0, test.doubleConfig.getExceptionally(), 0.0);
-            Assertions.assertEquals(1.0F, test.floatConfig.getExceptionally(), 0.0F);
-            Assertions.assertEquals(1, test.integerConfig.getExceptionally().intValue());
-            Assertions.assertEquals(TestEnum.BAD, test.enumConfig.getExceptionally());
+            Assertions.assertNull(test.stringConfig.getExceptionally());
+            Assertions.assertTrue(test.booleanConfig.getExceptionally());
+            Assertions.assertEquals(0.0, test.doubleConfig.getExceptionally(), 0.0);
+            Assertions.assertEquals(0.0F, test.floatConfig.getExceptionally(), 0.0F);
+            Assertions.assertEquals(0, test.integerConfig.getExceptionally().intValue());
+            Assertions.assertEquals(TestEnum.GOOD, test.enumConfig.getExceptionally());
         }catch (ConfigException e) {
             Assertions.fail(e.getMessage());
         }
