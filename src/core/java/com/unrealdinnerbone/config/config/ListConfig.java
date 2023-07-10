02ba4379c0c1ef342de34c480f82e19c15421352
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2023-07-09 20:33:52 -0500

    add and remove from list config

--- src/core/java/com/unrealdinnerbone/config/config/ListConfig.java
+++ src/core/java/com/unrealdinnerbone/config/config/ListConfig.java
@@ -7,15 +7,49 @@ import com.unrealdinnerbone.unreallib.Namespace;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+
 public class ListConfig<T> extends ConfigValue<T[]> {
 
 
     private final Class<T[]> clazz;
 
+    @Nullable
+    private Supplier<List<String>> examples;
+
     public ListConfig(Namespace id, IProvider provider, @Nullable T[] defaultValue, Class<T[]> clazz) {
         super(id, provider, defaultValue);
         this.clazz = clazz;
+    }
 
+    public void setExamples(Supplier<List<String>> examples) {
+        this.examples = examples;
+    }
+
+    public void addValue(T value) throws ConfigParseException {
+        Optional<T[]> optionalTS = get();
+        if(optionalTS.isPresent()) {
+            T[] a = optionalTS.get();
+            List<T> list = new ArrayList<>(Arrays.asList(a));
+            list.add(value);
+            T[] newArray = list.toArray(a);
+            set(newArray);
+        }
+    }
+
+    public void removeValue(T value) throws ConfigParseException {
+        Optional<T[]> optionalTS = get();
+        if(optionalTS.isPresent()) {
+            T[] a = optionalTS.get();
+            List<T> list = new ArrayList<>(Arrays.asList(a));
+            list.remove(value);
+            T[] newArray = list.toArray(a);
+            set(newArray);
+        }
     }
 
     @Override
