d2d186ec6106a11948726ea66174220dbae7c1f2
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2023-09-17 11:41:26 -0500

    Dont require annotations

--- src/gson/java/module-info.java
+++ src/gson/java/module-info.java
@@ -1,7 +1,6 @@
 module com.unrealdinnerbone.unrealconfig.gson {
     requires com.unrealdinnerbone.unrealconfig;
     requires com.google.gson;
-    requires org.jetbrains.annotations;
     requires org.slf4j;
     exports com.unrealdinnerbone.config.gson;
 }
