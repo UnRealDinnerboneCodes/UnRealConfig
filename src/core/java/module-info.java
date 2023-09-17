7b24ea81ef81a21816077ba021230f4fb01ba6fa
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2023-09-17 11:55:02 -0500

    Just remove them

--- src/core/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.unrealdinnerbone.unrealconfig {
-    requires static org.jetbrains.annotations;
-    exports com.unrealdinnerbone.config;
-    exports com.unrealdinnerbone.config.api;
-    exports com.unrealdinnerbone.config.config;
-    exports com.unrealdinnerbone.config.impl.provider;
-    exports com.unrealdinnerbone.config.exception;
-}
--- src/gson/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.unrealdinnerbone.unrealconfig.gson {
-    requires com.unrealdinnerbone.unrealconfig;
-    requires com.google.gson;
-    requires static org.jetbrains.annotations;
-    requires org.slf4j;
-    exports com.unrealdinnerbone.config.gson;
-}
